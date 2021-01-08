40d81e30c2e088047281f4584590877c0e9d65f6
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2021-01-08 16:20:16 -0500

    Day 1. Working with testing.
    Signed-off-by: Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>

--- docs/Go_test/playground/idea_for_testing/idea0/idea.go
+++ docs/Go_test/playground/idea_for_testing/idea0/idea.go
@@ -1,11 +1,25 @@
 package idea0
 
-import "strings"
-
+import (
+	"errors"
+	"strings"
+)
 
 func Fun(s string, sub string) string {
 	if strings.Contains(s, sub) {
 		return "match"
 	}
 	return "no match"
+}
+
+func Fun2(s string, sub string) (string, error) {
+
+	if s == "" || sub == "" {
+		return "", errors.New("Empty")
+	}
+
+	if strings.Contains(s, sub) {
+		return "match", nil
+	}
+	return "no match", nil
 }
--- docs/Go_test/playground/idea_for_testing/idea0/idea_test.go
+++ docs/Go_test/playground/idea_for_testing/idea0/idea_test.go
@@ -1,6 +1,8 @@
 package idea0
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFun(t *testing.T) {
 	type args struct {
@@ -13,13 +15,45 @@ func TestFun(t *testing.T) {
 		want string
 	}{
 		// TODO: Add test cases.
-		{name: "test valid", args: args{s: "this is fox",sub: "fox"}, want: "match"},
-		{name: "test valid", args: args{s: "this is",sub: "fox"}, want: "no match"},
+		{name: "test valid", args: args{s: "this is fox", sub: "fox"}, want: "match"},
+		{name: "test valid", args: args{s: "this is", sub: "fox"}, want: "no match"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Fun(tt.args.s, tt.args.sub); got != tt.want {
 				t.Errorf("Fun() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFun2(t *testing.T) {
+	type args struct {
+		s   string
+		sub string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		// TODO: Add test cases.
+		{name: "test valid", args: args{s: "this is fox", sub: "fox"}, want: "match",wantErr: false},
+		{name: "test valid", args: args{s: "this is", sub: "fox"}, want: "no match", wantErr: false},
+		{name: "test Error", args: args{s: "", sub: "fox"}, want: "", wantErr: true},
+
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Fun2(tt.args.s, tt.args.sub)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Fun2() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Fun2() = %v, want %v", got, tt.want)
 			}
 		})
 	}
